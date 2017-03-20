e2702c4749211cb8f9a87508026a9660c65538d0
人啊！！！ <s20151104690@1.local>
2017-03-20 12:00:39 +0800

    一个有问题的计算器
    还没有调试完成

--- calculatetest/ViewController.swift
+++ calculatetest/ViewController.swift
@@ -9,24 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func add(_ sender: Any) {
-    }
-    @IBOutlet weak var text: UITextField!
-    @IBAction func one(_ sender: Any) {
-    }
-    @IBAction func zero(_ sender: Any) {
-    }
+    @IBOutlet weak var resulttxt: UILabel!
+    var fristval:Double = 0
+    var sencendval:Double = 0
+    var fhval:Int = 0
+    var isfinash:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func setnum(_ sender: UIButton) {
+        if(resulttxt.text! != "0" && isfinash == false){
+            resulttxt.text! += String(sender.tag)
+        }else{
+            resulttxt.text! = String(sender.tag)
+            isfinash = false
+        }
+    }
+    
+    @IBAction func setfh(_ sender: UIButton) {
+        if(fristval == 0){
+            fristval = (resulttxt.text! as NSString).doubleValue
+        }else{
+            equal(sender)
+        }
+        fhval = sender.tag
+        isfinash = true
+    }
+    
+    
+    @IBAction func equal(_ sender: Any) {
+        if fristval != 0 && fhval != 0 {
+            sencendval = (resulttxt.text! as NSString).doubleValue
+            switch (fhval){
+            case 1:
+                fristval += sencendval
+            case 2:
+                fristval -= sencendval
+            case 3:
+                fristval *= sencendval
+            case 4:
+                fristval /= sencendval
+            default:
+                break
+            }
+            var result:String = String(fristval)
+            let len = result.characters.index(of:".")
+            let str = "0"+result.substring(from: len!)
+            
+            if ((str as NSString).doubleValue == 0)
+            {
+                result = result.substring(to: len!)
+            }
+            resulttxt.text! = result
+            fhval = 0
+            isfinash = true
+        }
+    }
+    
+    @IBAction func point(_ sender: Any) {
+        if(!resulttxt.text!.contains(".") && isfinash == false){
+            resulttxt.text! += "."
+        }
+    }
+    
 
 
-}
-
+    @IBAction func reset(_ sender: Any) {
+        resulttxt.text = "0"
+        fristval = 0
+        sencendval = 0
+        fhval = 0    }
+    }
